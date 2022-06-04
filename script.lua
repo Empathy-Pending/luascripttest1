61ce085384aad0091d47bd624b13c456a91c77b6
Empathy-Pending <89162352+Empathy-Pending@users.noreply.github.com>
2022-06-04 01:57:13 +0100

    Update script.lua

--- script.lua
+++ script.lua
@@ -1,83 +1,6 @@
 local a = loadstring(game:HttpGet("https://raw.githubusercontent.com/Empathy-Pending/luascripttest1/main/ui.lua"))()
-local e = a:Window("FE Commands")
+local e = a:Window("Main")
 
-
-e:Box(
-    "Kill Player",
-    function(l, m)
-        if m and game.Players[l] then
-            if game.Players[l].Character.Humanoid.Sit then
-                game:GetService("StarterGui"):SetCore(
-                    "SendNotification",
-                    {Title = "Kill Player Script", Text = "Failed Because Player is Sitting", Duration = 5}
-                )
-                return
-            end
-            yes = game:GetService("Players").LocalPlayer.Character.HumanoidRootPart.CFrame
-            pcall(
-                function()
-                    local g = {[1] = "GiveItem", [2] = 495886176}
-                    game:GetService("ReplicatedStorage").PlayerChannel:FireServer(unpack(g))
-                    wait(.20)
-                    game.Players.LocalPlayer.Character.Humanoid.Name = 1
-                    local h = game.Players.LocalPlayer.Character["1"]:Clone()
-                    h.Parent = game.Players.LocalPlayer.Character
-                    h.Name = "Humanoid"
-                    wait(0.1)
-                    game.Players.LocalPlayer.Character["1"]:Destroy()
-                    workspace.CurrentCamera.CameraSubject = game.Players[l].Character
-                    game.Players.LocalPlayer.Character.Animate.Disabled = true
-                    wait(0.1)
-                    game.Players.LocalPlayer.Character.Animate.Disabled = false
-                    game.Players.LocalPlayer.Character.Humanoid.DisplayDistanceType = "None"
-                    wait(.20)
-                    local i = "PaintBucket"
-                    for j, k in pairs(game:GetService("Players").LocalPlayer.Backpack:GetChildren()) do
-                        if k:IsA("Tool") and k.Name == i then
-                            k.Parent = game:GetService("Players").LocalPlayer.Character
-                        end
-                    end
-                    game:GetService("Players").LocalPlayer.Character.HumanoidRootPart.CFrame =
-                        game.Players[l].Character.HumanoidRootPart.CFrame
-                    wait(.10)
-                    game:GetService("Players").LocalPlayer.Character.HumanoidRootPart.CFrame =
-                        game.Players[l].Character.HumanoidRootPart.CFrame
-                    wait(.10)
-                    game:GetService("Players").LocalPlayer.Character.HumanoidRootPart.CFrame =
-                        game.Players[l].Character.HumanoidRootPart.CFrame
-                    wait(.10)
-                    game:GetService("Players").LocalPlayer.Character.HumanoidRootPart.CFrame =
-                        game.Players[l].Character.HumanoidRootPart.CFrame
-                    wait(.10)
-                    game:GetService("Players").LocalPlayer.Character.HumanoidRootPart.CFrame =
-                        game.Players[l].Character.Head.CFrame
-                    wait(.10)
-                    game:GetService("Players").LocalPlayer.Character.HumanoidRootPart.CFrame =
-                        game.Players[l].Character.HumanoidRootPart.CFrame
-                    wait(.10)
-                    game:GetService("Players").LocalPlayer.Character.HumanoidRootPart.CFrame =
-                        game.Players[l].Character.HumanoidRootPart.CFrame
-                    wait(.10)
-                    game:GetService("Players").LocalPlayer.Character.HumanoidRootPart.CFrame =
-                        game.Players[l].Character.Head.CFrame
-                    wait(.10)
-                    game:GetService("Players").LocalPlayer.Character.HumanoidRootPart.CFrame =
-                        game.Players[l].Character.HumanoidRootPart.CFrame
-                    wait(1)
-                    game:GetService("Players").LocalPlayer.Character.HumanoidRootPart.CFrame =
-                        CFrame.new(85.092697143555, -39.80192565918, 434.20581054688)
-                    wait(.05)
-                    game:GetService("Players").LocalPlayer.Character.HumanoidRootPart.CFrame =
-                        CFrame.new(85.092697143555, -39.80192565918, 434.20581054688)
-                    wait(5)
-                    game:GetService("Players").LocalPlayer.Character.HumanoidRootPart.CFrame = yes
-                end
-            )
-        end
-        wait(4)
-        game.Players.LocalPlayer.PlayerGui.MainGui.Menu.Backpack.Tools.Shortcut:Destroy()
-    end
-)
 e:Box(
     "Kick Player",
     function(q, r)
@@ -175,78 +98,12 @@ e:Box(
                     "SendNotification",
                     {
                         Title = "Kick Player Script",
-                        Text = game.Players[q].Name .. "   was died while kicking him, Try Kick him again",
+                        Text = game.Players[q].Name .. "   was dead while kicking him, Try Kick him again",
                         Duration = 5
                     }
                 )
                 return
             end
         end
-    end
-)
-e:Box(
-    "Bring Player",
-    function(o, p)
-        if p and game.Players[o] then
-            if game.Players[o].Character.Humanoid.Sit then
-                game:GetService("StarterGui"):SetCore(
-                    "SendNotification",
-                    {Title = "Bring Player Script", Text = "Failed Because Player is Sitting", Duration = 5}
-                )
-                return
-            end
-            yes = game:GetService("Players").LocalPlayer.Character.HumanoidRootPart.CFrame
-            pcall(
-                function()
-                    local g = {[1] = "GiveItem", [2] = 495886176}
-                    game:GetService("ReplicatedStorage").PlayerChannel:FireServer(unpack(g))
-                    wait(.20)
-                    game.Players.LocalPlayer.Character.Humanoid.Name = 1
-                    local h = game.Players.LocalPlayer.Character["1"]:Clone()
-                    h.Parent = game.Players.LocalPlayer.Character
-                    h.Name = "Humanoid"
-                    wait(0.1)
-                    game.Players.LocalPlayer.Character["1"]:Destroy()
-                    workspace.CurrentCamera.CameraSubject = game.Players[o].Character
-                    game.Players.LocalPlayer.Character.Animate.Disabled = true
-                    wait(0.1)
-                    game.Players.LocalPlayer.Character.Animate.Disabled = false
-                    game.Players.LocalPlayer.Character.Humanoid.DisplayDistanceType = "None"
-                    wait(.20)
-                    local i = "PaintBucket"
-                    for j, k in pairs(game:GetService("Players").LocalPlayer.Backpack:GetChildren()) do
-                        if k:IsA("Tool") and k.Name == i then
-                            k.Parent = game:GetService("Players").LocalPlayer.Character
-                        end
-                    end
-                    game:GetService("Players").LocalPlayer.Character.HumanoidRootPart.CFrame =
-                        game.Players[o].Character.HumanoidRootPart.CFrame
-                    wait(.05)
-                    game:GetService("Players").LocalPlayer.Character.HumanoidRootPart.CFrame =
-                        game.Players[o].Character.HumanoidRootPart.CFrame
-                    wait(.05)
-                    game:GetService("Players").LocalPlayer.Character.HumanoidRootPart.CFrame =
-                        game.Players[o].Character.Head.CFrame
-                    wait(.05)
-                    game:GetService("Players").LocalPlayer.Character.HumanoidRootPart.CFrame =
-                        game.Players[o].Character.HumanoidRootPart.CFrame
-                    wait(.05)
-                    game:GetService("Players").LocalPlayer.Character.HumanoidRootPart.CFrame =
-                        game.Players[o].Character.Head.CFrame
-                    wait(.05)
-                    game:GetService("Players").LocalPlayer.Character.HumanoidRootPart.CFrame =
-                        game.Players[o].Character.HumanoidRootPart.CFrame
-                    wait(.05)
-                    game:GetService("Players").LocalPlayer.Character.HumanoidRootPart.CFrame =
-                        game.Players[o].Character.HumanoidRootPart.CFrame
-                    wait(.05)
-                    wait(1)
-                    game:GetService("Players").LocalPlayer.Character.HumanoidRootPart.CFrame = yes
-                end
-            )
-        end
-        wait(5)
-        game.Players.LocalPlayer.PlayerGui.MainGui.Menu.Backpack.Tools.Shortcut:Destroy()
-        game:GetService("Players").LocalPlayer.Character.HumanoidRootPart.CFrame = yes
     end
 )
